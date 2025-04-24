adfa9d7ccf1b114368da26905d21559a00c4bbe5
Gage Joshua Hepworth <ghepwort@uwyo.edu>
2025-04-24 12:44:55 -0600

    Tried it on an online JS code runner and it worked there. Commiting now and will run it on my personal machine later tonight.

--- code.js
+++ code.js
@@ -1,3 +1,18 @@
-function augmentingPath(graph, start, end) {
-    return [];
-}
+function findAugmentingPath(graph, currentNode, targetNode, visited = new Set()) { 
+    if (!(currentNode in graph)) return []; 
+
+    if (currentNode === targetNode) return [currentNode]; 
+
+    visited.add(currentNode); 
+
+    for (const neighbor in graph[currentNode]) { 
+        const capacity = graph[currentNode][neighbor]; 
+        if (capacity > 0 && !visited.has(neighbor)) { 
+            const path = findAugmentingPath(graph, neighbor, targetNode, visited); 
+            if (path.length > 0) { 
+                return [currentNode, ...path]; 
+            } 
+        } 
+    } 
+    return []; 
+} 
